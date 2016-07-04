c6623cf8fc9484d5e344808b7aad5f46e0894347
Andrew Duthie <aduthie7@gmail.com>
2016-07-04 17:24:11 -0400

    Incorporate full HTML layout

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php wp_head(); ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
 <div id="app"></div>
+
 <?php wp_footer(); ?>
+
+</body>
+</html>
