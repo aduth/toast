ec0b15d9e151f1588dc3d21b7971d2ad2927788e
Andrew Duthie <aduthie7@gmail.com>
2016-07-05 21:46:10 -0400

    Implement and incorporate QueryCrumbs component

---
+++ src/components/query-crumbs/index.js
@@ -0,0 +1,32 @@
+/**
+ * External dependencies
+ */
+import { Component } from 'preact';
+import { connect } from 'preact-redux';
+
+/**
+ * Internal dependencies
+ */
+import { requestCrumbs } from 'state/crumbs/actions';
+import { isRequestingCrumbs } from 'state/crumbs/selectors';
+
+class QueryCrumbs extends Component {
+	componentWillMount() {
+		if ( ! this.props.requesting ) {
+			this.props.requestCrumbs();
+		}
+	}
+
+	render() {
+		return null;
+	}
+}
+
+export default connect(
+	( state ) => {
+		return {
+			requesting: isRequestingCrumbs( state )
+		};
+	},
+	{ requestCrumbs }
+)( QueryCrumbs );
--- src/sections/home/index.js
+++ src/sections/home/index.js
@@ -7,11 +7,13 @@ import { h } from 'preact';
  * Internal dependencies
  */
 import DocumentHead from 'components/document-head';
+import QueryCrumbs from 'components/query-crumbs';
 
 export default function() {
 	return (
 		<div>
 			<DocumentHead title="" />
+			<QueryCrumbs />
 			Home
 		</div>
 	);
