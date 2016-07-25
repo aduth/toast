ba128607df6d11d2630d81ad0dd6d36855d3c54c
Andrew Duthie <aduthie7@gmail.com>
2016-07-25 19:55:48 -0400

    Remove recompose dependency
    Requires React as peerDependency

--- src/components/document-head/index.js
+++ src/components/document-head/index.js
@@ -1,20 +1,15 @@
 /**
- * External dependencies
- */
-import pure from 'recompose/pure';
-
-/**
  * Internal dependencies
  */
 import { SITE_NAME } from 'config';
 
-function DocumentHead( { title } ) {
+export default function DocumentHead( { title } ) {
 	let nextTitle = SITE_NAME;
 	if ( title ) {
 		nextTitle = title + ' - ' + nextTitle;
 	}
 
-	document.title = nextTitle;
+	if ( document.title !== nextTitle ) {
+		document.title = nextTitle;
+	}
 }
-
-export default pure( DocumentHead );
