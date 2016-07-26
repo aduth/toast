bb88d628925e0ac55308ea6a5db770df9967aa86
Andrew Duthie <aduthie7@gmail.com>
2016-07-25 20:00:30 -0400

    Remove react-hot-loader in favor of plain HMR
    Already used in initialization step anyways

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -11,7 +11,6 @@ var common = require( './webpack.config.common' );
 
 module.exports = merge( common, {
 	entry: [
-		'react-hot-loader/patch',
 		'webpack-dev-server/client?http://localhost:3000',
 		'webpack/hot/only-dev-server'
 	].concat( common.entry ),
